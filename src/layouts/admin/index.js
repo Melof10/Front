af5fe6fdde98ef69f3f2f5527ab1c7f5fdd8b738
Melof10 <95fedemelo@gmail.com>
2021-05-22 20:27:14 -0300

    Created layouts

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React  from 'react';
 
 function NotFound(){
     return(        
-        <h1>ERROR 404 - NOT FOUND</h1>        
+        <h1>Layout Admin</h1>        
     )
 }
 

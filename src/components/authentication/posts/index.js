8ba6d277d53580a801f8e4e5b3aa519a0a1fedde
Melof10 <95fedemelo@gmail.com>
2021-05-22 20:19:30 -0300

    Created screens

---
+++ src/components/authentication/posts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const index = () => {
+    return(
+        <h1>Posts</h1>
+    )
+}
+
+export default index;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+function NotFound(){
+    return(        
+        <h1>ERROR 404 - NOT FOUND</h1>        
+    )
+}
+
+export default NotFound;

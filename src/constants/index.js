164ea22bcd87df7eed3f0a0981267c50a05019f3
Melof10 <95fedemelo@gmail.com>
2021-05-22 23:45:27 -0300

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    /***** BASE URL *****/
+    API_URL: 'http://localhost:3000',
+
+    /***** ROUTE for request *****/
+    LOGIN: '/users',
+
+    /***** METHOD for request *****/
+    REQUEST_METHOD_GET: 'GET',
+    REQUEST_METHOD_POST: 'POST',
+    REQUEST_METHOD_PATCH: 'PATCH'
+}

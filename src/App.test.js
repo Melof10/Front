b360acbe100642a7020b377c4f09084d5d4db0ec
Melof10 <95fedemelo@gmail.com>
2021-05-24 15:27:09 -0300

    Added test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './index';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe('render App', () => {
+    test('matches snapshot', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toMatchSnapshot();
+    });
 });

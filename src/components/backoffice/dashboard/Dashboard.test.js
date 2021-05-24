7211dcec4ff87ae4da4be64dfc37384778521998
Melof10 <95fedemelo@gmail.com>
2021-05-24 15:32:25 -0300

    Added test for Dashboard

--- src/components/backoffice/dashboard/Dashboard.test.js
+++ src/components/backoffice/dashboard/Dashboard.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import Dashboard from './index';
 
 configure({ adapter: new Adapter() });
 
-describe('render App', () => {
+describe('render Dashboard', () => {
     test('matches snapshot', () => {
-        const wrapper = shallow(<App />);
+        const wrapper = shallow(<Dashboard />);
         expect(wrapper).toMatchSnapshot();
     });
 });
